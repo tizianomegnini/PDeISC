cd964f50972f88375538e9b40e998f419bbe01fc
tizianomegnini <134891302+tizianomegnini@users.noreply.github.com>
2026-04-23 19:51:31 -0300

    tarea

--- 1_NodeJS/NJS3/punto2/server.js
+++ 1_NodeJS/NJS3/punto2/server.js
@@ -5,17 +5,15 @@ import { fileURLToPath } from "url";
 const app = express();
 const PORT = 3000;
 
-// Esto reemplaza __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Servir archivos sin carpeta public
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/pages/page.html"));
+    res.sendFile(path.join(__dirname, "/pages/menu.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
